f17262b5b7a828781693ea734cc9f2e416d30a50
Deependra Pratap Singh <sachinswati51@gmail.com>
2026-05-04 23:36:40 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Smart Attendance System</title>
+</head>
+<body>
+
+    <h1>Smart Attendance System</h1>
+
+    <a href="/register"><button>Register Student</button></a><br><br>
+    <a href="/dataset"><button>Create Dataset</button></a><br><br>
+    <a href="/train"><button>Train Model</button></a><br><br>
+    <a href="/recognize"><button>Start Attendance</button></a><br><br>
+    <a href="/dashboard"><button>View Dashboard</button></a>
+
+</body>
+</html>
