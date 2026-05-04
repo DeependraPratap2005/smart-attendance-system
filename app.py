d3ac6ab2b40a8660ccda3dac8ccd81f151180e9a
Deependra Pratap Singh <sachinswati51@gmail.com>
2026-05-04 23:45:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Smart Attendance System</title>
-</head>
-<body>
+from flask import Flask, render_template
+import os
 
-    <h1>Smart Attendance System</h1>
+app = Flask(__name__)
 
-    <a href="/register"><button>Register Student</button></a><br><br>
-    <a href="/dataset"><button>Create Dataset</button></a><br><br>
-    <a href="/train"><button>Train Model</button></a><br><br>
-    <a href="/recognize"><button>Start Attendance</button></a><br><br>
-    <a href="/dashboard"><button>View Dashboard</button></a>
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-</body>
-</html>
+@app.route("/register")
+def register():
+    os.system("python register_student.py")
+    return "Student Registered!"
+
+@app.route("/dataset")
+def dataset():
+    os.system("python dataset_creator.py")
+    return "Dataset Created!"
+
+@app.route("/train")
+def train():
+    os.system("python train_model.py")
+    return "Model Trained!"
+
+@app.route("/recognize")
+def recognize():
+    os.system("python recognize.py")
+    return "Attendance Started!"
+
+@app.route("/dashboard")
+def dashboard():
+    os.system("python dashboard.py")
+    return "Dashboard Opened!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
